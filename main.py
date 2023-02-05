7fc842880c66a3cc1802ad7d5bcf43e9fd9f6937
Mathieu NICOLLE <mnicolle.utc@gmail.com>
2023-02-05 15:30:15 +0100

    Initiate structure of git repository

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 11:39:57 2022
+Description of the program available at :
+https://github.com/mnicolleUTC/Phylogenetic-Analyses.git
+@author: Nicolle Mathieu
+"""
+
+import os
+
+
+if __name__=="__main__":
+     print("coucou")
+
